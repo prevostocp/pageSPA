f444e26dc7927513f4d40c645130a4b6e0195c99
prevostocp <dvdcryptocba@gmail.com>
2023-09-11 08:26:47 -0300

    Lunes 11/9 cambios header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,18 +1,7 @@
 import { Sidebar } from "./Sidebar.js";
-import { Title } from "./Title.js";
 
 export function Header() {
   const $header = document.createElement("header");
-  // const $divContainer = document.createElement("div");
-  // $divContainer.classList.add(
-  //   "container-fluid",
-  //   "position-relative",
-  //   "d-flex",
-  //   "p-0",
-  //   "bg-info"
-  // );
-  // $divContainer.appendChild(Title());
-
   $header.appendChild(Sidebar());
 
   return $header;
